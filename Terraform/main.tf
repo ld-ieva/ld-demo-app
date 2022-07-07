c8183ec6ca635536625deb2302ae9d51e31df190
ld-ieva <108881843+ld-ieva@users.noreply.github.com>
2022-07-07 10:33:09 -0600

    Updated name of terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -15,8 +15,8 @@ provider "launchdarkly" {
 }
 
 resource "launchdarkly_project" "terraform" {
-  key  = "cody-ld-demo-tf"
-  name = "cody-ld-demo-tf"
+  key  = "ieva-ld-demo-tf"
+  name = "ieva-ld-demo-tf"
 
   tags = [
     "terraform",
